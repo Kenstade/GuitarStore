ce402bc2f16c6ddd36e91abfa5a04d44a314e3c8
kenstade <kenstade@mail.ru>
2025-03-31 18:52:39 +0300

    Update and rename ShoppingCartModule

--- src/Modules/GuitarStore.Modules.ShoppingCart/Data/CartDbContext.cs
+++ src/Modules/GuitarStore.Modules.ShoppingCart/Data/CartDbContext.cs
@@ -1,8 +1,8 @@
-﻿using GuitarStore.Modules.ShoppingCarts.Models;
+﻿using GuitarStore.Modules.ShoppingCart.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace GuitarStore.Modules.ShoppingCarts.Data;
-public sealed class CartsDbContext(DbContextOptions<CartsDbContext> options) : DbContext(options)
+namespace GuitarStore.Modules.ShoppingCart.Data;
+public sealed class CartDbContext(DbContextOptions<CartDbContext> options) : DbContext(options)
 {
     public DbSet<Cart> Carts { get; set; }
     public DbSet<CartItem> CartItems { get; set; }
@@ -10,7 +10,7 @@ public sealed class CartsDbContext(DbContextOptions<CartsDbContext> options) : D
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("cart");
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CartsDbContext).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CartDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
     }
 }
