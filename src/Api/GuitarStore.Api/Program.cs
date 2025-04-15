dd0ba1a8027f0929d09aac5d7286343887a98f36
kenstade <kenstade@mail.ru>
2025-04-15 10:48:01 +0300

    Update masstransit registration

--- src/Api/GuitarStore.Api/Program.cs
+++ src/Api/GuitarStore.Api/Program.cs
@@ -15,13 +15,18 @@ using Hangfire;
 using BuildingBlocks.Core.Hangfire;
 using BuildingBlocks.Core.Messaging;
 using BuildingBlocks.Web.MinimalApi;
+using MassTransit;
 using Serilog;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
-builder.Services.AddMessageBroker(typeof(CatalogModule).Assembly, typeof(ShoppingCartModule).Assembly);
+
+builder.Services.AddMessageBus(
+    typeof(CatalogModule).Assembly,
+    typeof(ShoppingCartModule).Assembly);
+
 
 builder.Services.AddPostgresDbContext<MessageDbContext>(builder.Configuration);
 builder.Services.AddDistributedCache(builder.Configuration);
--- src/BuildingBlocks/BuildingBlocks.Core/Messaging/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Messaging/Extensions.cs
@@ -6,23 +6,31 @@ namespace BuildingBlocks.Core.Messaging;
 
 public static class Extensions
 {
-    public static IServiceCollection AddMessageBroker(this IServiceCollection services, params Assembly[] assemblies)
+    public static IServiceCollection AddMessageBus(this IServiceCollection services, params Assembly[] assemblies)
     {
+        //регистрация internal consumers. Сделать конфигурацию mt отельно в каждом модуле без автомат регистрации. 
+        var consumers = assemblies
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(t => t.IsClass && !t.IsAbstract && 
+                t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IConsumer<>))
+            ).ToArray();
+        
         services.AddMassTransit(busCfg =>
         {
             busCfg.SetKebabCaseEndpointNameFormatter();
-            busCfg.AddConsumers(assemblies);
+            busCfg.AddConsumers(consumers);
             
             busCfg.UsingInMemory((context, cfg) =>
             {
                 cfg.ConfigureEndpoints(context);
             });
+
+            busCfg.AddMediator(mCfg =>
+            {
+                mCfg.AddConsumers(consumers);
+            });
         }); 
 
-        services.AddMediator(cfg =>
-        {
-            cfg.AddConsumers(assemblies);
-        });
         return services;
     }
 }
