f1d0c8b0f97ac7f9058caaa72360ea44384b9fd2
kenstade <kenstade@mail.ru>
2025-04-18 11:19:37 +0300

    Update ModuleExtensions.cs

--- src/Api/GuitarStore.Api/Extensions/ModuleExtensions.cs
+++ src/Api/GuitarStore.Api/Extensions/ModuleExtensions.cs
@@ -2,18 +2,15 @@ namespace GuitarStore.Api.Extensions;
 
 internal static class ModuleExtensions
 {
-    public static void AddModulesSettingsFile(this ConfigurationManager configurationManager, string root,
+    internal static void AddModulesSettingsFile(this ConfigurationManager configurationManager, string root,
         string environment)
     {
-        foreach (string file in Directory.GetFiles(root, "*.appsettings.json", SearchOption.AllDirectories))
+        foreach (var file in Directory.GetFiles(root, "*.appsettings.json", SearchOption.AllDirectories))
         {
             configurationManager.AddJsonFile(file);
         }
         
-        foreach (string file in Directory.GetFiles(
-                     root,
-                     $"*.appsettings.{environment}.json",
-                     SearchOption.AllDirectories))
+        foreach (var file in Directory.GetFiles(root,$"*.appsettings.{environment}.json",SearchOption.AllDirectories))
         {
             configurationManager.AddJsonFile(file, true, true);
         }
