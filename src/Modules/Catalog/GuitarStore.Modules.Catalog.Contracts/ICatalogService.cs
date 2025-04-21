c09ddc5016cdfc359f7f7390d23830d25bc0c481
kenstade <kenstade@mail.ru>
2025-04-21 15:28:05 +0300

    Add Catalog.Contracts class library

---
+++ src/Modules/Catalog/GuitarStore.Modules.Catalog.Contracts/ICatalogService.cs
@@ -0,0 +1,8 @@
+﻿namespace GuitarStore.Modules.Catalog.Contracts;
+
+public interface ICatalogService
+{
+    Task<ProductDetails?> GetProductForCartAsync(Guid productId, CancellationToken ct = default);
+}
+
+public sealed record ProductDetails(string Name, string? Image, decimal Price);
