06140ffda48114c9ad47b47899a39936d8ccdfda
kenstade <kenstade@mail.ru>
2025-04-27 19:54:02 +0300

    Remove unused stuff

--- src/Modules/Identity/GuitarStore.Modules.Identity/Models/UserRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace GuitarStore.Modules.Identity.Models;
-
-public class UserRole : IdentityUserRole<Guid>
-{
-    public virtual User? User { get; set; }
-    public virtual Role? Role { get; set; }
-}
