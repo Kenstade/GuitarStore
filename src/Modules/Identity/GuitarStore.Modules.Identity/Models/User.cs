9336cd77292bca95e9fd9299dab23f1e436b6abf
kenstade <kenstade@mail.ru>
2025-04-27 19:59:20 +0300

    Update User.cs

--- src/Modules/Identity/GuitarStore.Modules.Identity/Models/User.cs
+++ src/Modules/Identity/GuitarStore.Modules.Identity/Models/User.cs
@@ -1,32 +1,41 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using BuildingBlocks.Core.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using IdentityDbContext = GuitarStore.Modules.Identity.Data.IdentityDbContext;
 
 namespace GuitarStore.Modules.Identity.Models;
 
-public class User : IdentityUser<Guid>
-{
-    //TODO: add first name and last name ?
-    public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = default!;
-    public virtual ICollection<UserRole> UserRoles { get; set; } = default!;
-    public DateTime CreatedAt { get; set; }
+internal sealed class User : Aggregate<Guid>
+{   
+    public string Email { get; private set; } = null!;
+    public string Password { get; private set; } = null!;
+    public string PhoneNumber { get; private set; } = null!;
+
+    public static User Create(string email, string password)
+    {
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Email = email,
+            Password = password,
+        };
+        //user registered domain event
+        return user;
+    }
 }
 
-public class UserConfiguration : IEntityTypeConfiguration<User>
+internal class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
+        builder.ToTable("users", IdentityDbContext.DefaultSchema);
         builder.HasKey(u => u.Id);
-
-        builder.Property(u => u.UserName).HasMaxLength(50).IsRequired();
+        
         builder.Property(u => u.Email).HasMaxLength(50).IsRequired();
-        builder.Property(u => u.PhoneNumber).HasMaxLength(15).IsRequired(false);
-
         builder.HasIndex(u => u.Email).IsUnique();
-
-        builder.HasMany(u => u.UserRoles)
-            .WithOne(ur => ur.User)
-            .HasForeignKey(ur => ur.UserId)
-            .IsRequired();
+        
+        builder.Property(u => u.Password).HasMaxLength(50).IsRequired();
+        
+        builder.Property(u => u.PhoneNumber).HasMaxLength(15).IsRequired(false);
     }
 }
