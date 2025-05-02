84c0f82276b90ac465cd8ad9763e14e785167cc0
kenstade <kenstade@mail.ru>
2025-05-02 20:25:53 +0300

    Update JwtOptions.cs

--- src/BuildingBlocks/BuildingBlocks.Core/Security/JwtOptions.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Security/JwtOptions.cs
@@ -2,7 +2,7 @@
 
 public sealed class JwtOptions
 {
-    public string Audience { get; set; } = default!;
-    public string MetadataAddress { get; set; } = default!;
-    public string[] ValidIssuer { get; set; } = default!;
+    public string Audience { get; set; } = null!;
+    public string MetadataAddress { get; set; } = null!;
+    public string[] ValidIssuer { get; set; } = null!;
 }
