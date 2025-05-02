6c62c932e279fe6727ad4973ea5fdb84e7567a06
kenstade <kenstade@mail.ru>
2025-05-02 20:20:01 +0300

    Update KeyCloakOptions.cs

---
+++ src/BuildingBlocks/BuildingBlocks.Core/Security/KeyCloakOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildingBlocks.Core.Security;
+
+public sealed class KeyCloakOptions
+{
+    public string AuthorizationUrl { get; set; } = null!;
+    public string HealthUrl { get; set; } = null!;
+    public string AdminUrl { get; set; } = null!;
+    public string TokenUrl { get; set; } = null!;
+    public string ConfidentialClientId { get; set; } = null!;
+    public string ConfidentialClientSecret { get; set; } = null!;
+    public string PublicClientId {get; set;} = null!;
+}
--- src/BuildingBlocks/BuildingBlocks.Core/Security/KeycloakOptions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BuildingBlocks.Core.Security;
-
-public sealed class KeycloakOptions
-{
-    public string AuthorizationUrl { get; set; } = default!;
-}
