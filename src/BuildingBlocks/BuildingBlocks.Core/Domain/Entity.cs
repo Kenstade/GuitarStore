2273ad08bbf34e37ad550f3247c06d57af500e2e
kenstade <kenstade@mail.ru>
2025-05-07 22:00:36 +0300

    Add non-generic Entity

--- src/BuildingBlocks/BuildingBlocks.Core/Domain/Entity.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Domain/Entity.cs
@@ -1,8 +1,13 @@
-﻿
-namespace BuildingBlocks.Core.Domain;
+﻿namespace BuildingBlocks.Core.Domain;
+
 public abstract class Entity<TId> : IEntity<TId>
 {
     public TId Id { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
+public abstract class Entity : IEntity
+{
     public DateTime? CreatedAt { get; set; }
     public bool IsDeleted { get; set; }
 }
