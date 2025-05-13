2ecc8eb235fcd853c187fd656f4b10f947fa95fa
kenstade <kenstade@mail.ru>
2025-05-13 21:47:32 +0300

    Remove MassTransit Mediator and reflection

--- src/BuildingBlocks/BuildingBlocks.Core/Messaging/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Messaging/Extensions.cs
@@ -8,26 +8,14 @@ public static class Extensions
 {
     public static IServiceCollection AddMessageBus(this IServiceCollection services, params Assembly[] assemblies)
     {
-        //регистрация internal consumers. Сделать конфигурацию mt отельно в каждом модуле без автомат регистрации. 
-        var consumers = assemblies
-            .SelectMany(assembly => assembly.GetTypes())
-            .Where(t => t.IsClass && !t.IsAbstract && 
-                t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IConsumer<>))
-            ).ToArray();
-        
         services.AddMassTransit(busCfg =>
         {
             busCfg.SetKebabCaseEndpointNameFormatter();
-            busCfg.AddConsumers(consumers);
+            busCfg.AddConsumers(assemblies);
             
             busCfg.UsingInMemory((context, cfg) =>
             {
                 cfg.ConfigureEndpoints(context);
-            });
-
-            busCfg.AddMediator(mCfg =>
-            {
-                mCfg.AddConsumers(consumers);
             });
         }); 
 
