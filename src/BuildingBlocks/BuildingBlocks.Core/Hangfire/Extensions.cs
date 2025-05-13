da3be74a924cfb2056aeaba61ae67222fe25a244
kenstade <kenstade@mail.ru>
2025-05-13 21:45:53 +0300

    Remove Hangfire ProcessOutboxMessageJob scoped service and IApplicationBuilder extension

--- src/BuildingBlocks/BuildingBlocks.Core/Hangfire/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Hangfire/Extensions.cs
@@ -1,11 +1,8 @@
 ﻿using BuildingBlocks.Core.EFCore;
 using BuildingBlocks.Core.Extensions;
-using BuildingBlocks.Core.Messaging;
-using BuildingBlocks.Core.Messaging.Outbox;
 using Hangfire;
 using Hangfire.MemoryStorage;
 using Hangfire.PostgreSql;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,8 +13,7 @@ public static class Extensions
     public static IServiceCollection AddCustomHangfire(this IServiceCollection services, IConfiguration configuration)
     {
         var postgresOptions = configuration.GetOptions<PostgresOptions>(nameof(PostgresOptions));
-        if (postgresOptions == null) throw new ArgumentNullException(nameof(postgresOptions));
-
+        
         services.AddHangfire(config => 
         {
             if (!postgresOptions.UseInMemory)
@@ -29,24 +25,9 @@ public static class Extensions
                 config.UseMemoryStorage();
             }
         });
-
+        
         services.AddHangfireServer(options => options.SchedulePollingInterval = TimeSpan.FromSeconds(1));
-
-        services.AddScoped<ProcessOutboxMessagesJob>();
-
+        
         return services;
-    }
-
-    public static IApplicationBuilder UseBackgroundJobs(this IApplicationBuilder app, IConfiguration configuration)
-    {
-        var hangfireOptions = configuration.GetOptions<HangfireOptions>(nameof(HangfireOptions));
-        if (hangfireOptions == null) throw new ArgumentNullException(nameof(hangfireOptions));
-
-        app.ApplicationServices
-            .GetRequiredService<IRecurringJobManager>()
-            .AddOrUpdate<ProcessOutboxMessagesJob>("outbox-processor", job => 
-            job.ProcessAsync(), hangfireOptions.OutboxSchedule);
-
-        return app;
     }
 }
