8ff1ea18086f1b008a1ec1553d7a76d1921ca3f3
kenstade <kenstade@mail.ru>
2025-05-16 19:31:33 +0300

    Update IdentityModule.cs

--- src/Modules/Identity/GuitarStore.Modules.Identity/IdentityModule.cs
+++ src/Modules/Identity/GuitarStore.Modules.Identity/IdentityModule.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BuildingBlocks.Core.EFCore;
+using BuildingBlocks.Web.MinimalApi;
+using FluentValidation;
+using GuitarStore.Modules.Identity.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GuitarStore.Modules.Identity;
 public static class IdentityModule
 {
+    public const string ModuleName = "Identity";
     public static IServiceCollection AddIdentityModule(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddPostgresDbContext<IdentityContext>(configuration);
+        services.AddScoped<IDataSeeder, IdentityDataSeeder>();
+        services.AddValidatorsFromAssembly(typeof(IdentityModule).Assembly);
+        services.AddMinimalApiEndpoints(typeof(IdentityModule).Assembly);
+        
         return services;
+    }
+    public static IApplicationBuilder UseIdentityModule(this IApplicationBuilder app, IConfiguration configuration)
+    {
+        app.UseMigration<IdentityContext>();
+
+        return app;
     }
 }
