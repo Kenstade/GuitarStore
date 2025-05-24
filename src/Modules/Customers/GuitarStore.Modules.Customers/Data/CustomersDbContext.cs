685a79c556199209b60d79996cd73a8649f827c6
kenstade <kenstade@mail.ru>
2025-05-24 21:40:44 +0300

    Update CustomersDbContext.cs

--- src/Modules/Customers/GuitarStore.Modules.Customers/Data/CustomersDbContext.cs
+++ src/Modules/Customers/GuitarStore.Modules.Customers/Data/CustomersDbContext.cs
@@ -5,14 +5,17 @@ using Microsoft.EntityFrameworkCore;
 namespace GuitarStore.Modules.Customers.Data;
 internal sealed class CustomersDbContext(DbContextOptions<CustomersDbContext> options) : DbContext(options)
 {
+    public const string DefaultSchema = "customers";
+    
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Address> Addresses { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.HasDefaultSchema("customer");
+        modelBuilder.HasDefaultSchema(DefaultSchema);
         
+        modelBuilder.ApplyConfiguration(new OutboxMessageConfiguration());
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomersDbContext).Assembly);
-        modelBuilder.ConfigureOutboxMessage();
+        
         base.OnModelCreating(modelBuilder);
     }
 }
