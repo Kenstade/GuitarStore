0939cb2ff498dc191f2ae0cf3621de36bca90041
kenstade <kenstade@mail.ru>
2025-06-02 19:35:43 +0300

    Add Constants.cs with roles for IdentityModule

---
+++ src/Modules/Identity/GuitarStore.Modules.Identity/Constants.cs
@@ -0,0 +1,10 @@
+namespace GuitarStore.Modules.Identity;
+
+internal static class Constants
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
