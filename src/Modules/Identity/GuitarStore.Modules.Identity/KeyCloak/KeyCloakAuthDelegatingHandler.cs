7bc641150535c0ac80c0422b1cc1fed61f0a4fad
kenstade <kenstade@mail.ru>
2025-06-05 21:16:14 +0300

    Add KeyCloak client and IdentityProvider service

---
+++ src/Modules/Identity/GuitarStore.Modules.Identity/KeyCloak/KeyCloakAuthDelegatingHandler.cs
@@ -0,0 +1,53 @@
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using BuildingBlocks.Core.Security;
+using Microsoft.Extensions.Options;
+
+namespace GuitarStore.Modules.Identity.KeyCloak;
+
+internal sealed class KeyCloakAuthDelegatingHandler(IOptions<KeyCloakOptions> options) : DelegatingHandler
+{
+    private readonly KeyCloakOptions _options = options.Value;
+    
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        var authToken = await GetAuthorizationToken(cancellationToken);
+        
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authToken.AccessToken);
+        
+        var httpResponseMessage = await base.SendAsync(request, cancellationToken);
+        
+        httpResponseMessage.EnsureSuccessStatusCode();
+        
+        return httpResponseMessage;
+    }
+
+    private async Task<AuthToken> GetAuthorizationToken(CancellationToken cancellationToken)
+    {
+        var authRequestParams = new KeyValuePair<string, string>[]
+        {
+            new("client_id", _options.ConfidentialClientId),
+            new("client_secret", _options.ConfidentialClientSecret),
+            new("scope", "openid"),
+            new("grant_type", "client_credentials")
+        };
+        
+        using var authRequestContent = new FormUrlEncodedContent(authRequestParams);
+        using var authRequest = new HttpRequestMessage(HttpMethod.Post, new Uri(_options.TokenUrl));
+        
+        authRequest.Content = authRequestContent;
+        
+        using var authorizationResponse = await base.SendAsync(authRequest, cancellationToken);
+        
+        authorizationResponse.EnsureSuccessStatusCode();
+        
+        return await authorizationResponse.Content.ReadFromJsonAsync<AuthToken>(cancellationToken);
+    }
+    internal sealed class AuthToken
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; init; } = null!;
+    }
+}
