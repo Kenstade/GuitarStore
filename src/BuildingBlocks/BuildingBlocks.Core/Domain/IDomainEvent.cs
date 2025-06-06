c4df6b86b4207d50017a6fe5e63bc6830025fb78
kenstade <kenstade@mail.ru>
2025-06-06 20:02:37 +0300

    Update IDomainEvent.cs

--- src/BuildingBlocks/BuildingBlocks.Core/Domain/IDomainEvent.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Domain/IDomainEvent.cs
@@ -1,6 +1,3 @@
 ﻿namespace BuildingBlocks.Core.Domain;
-public interface IDomainEvent
-{ 
-    Guid Id => Guid.NewGuid();
-    public DateTime OccuredOn => DateTime.UtcNow;
-}
+
+public interface IDomainEvent;
