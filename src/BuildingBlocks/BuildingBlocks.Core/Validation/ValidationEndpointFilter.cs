603ce5b90f2a3f6ceb7f5bb65c670cf8aa00a32b
kenstade <kenstade@mail.ru>
2025-06-06 19:56:43 +0300

    Update ValidationEndpointFilter.cs

--- src/BuildingBlocks/BuildingBlocks.Core/Validation/ValidationEndpointFilter.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Validation/ValidationEndpointFilter.cs
@@ -3,20 +3,16 @@ using Microsoft.AspNetCore.Http;
 
 namespace BuildingBlocks.Core.Validation;
 
-public sealed class ValidationEndpointFilter<TRequest> : IEndpointFilter
+public sealed class ValidationEndpointFilter<TRequest>(IValidator<TRequest> validator) : IEndpointFilter
 {
-    private readonly IValidator<TRequest> _validator;
-    public ValidationEndpointFilter(IValidator<TRequest> validator)
-    {
-        _validator = validator;
-    }
     public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
         var request = context.GetArgument<TRequest>(0);
         
-        var result = await _validator.ValidateAsync(request);
-        if (!result.IsValid) return TypedResults.ValidationProblem(result.ToDictionary());
+        var result = await validator.ValidateAsync(request);
         
-        return await next(context);    
+        return result.IsValid 
+            ? await next(context) 
+            : Results.ValidationProblem(result.ToDictionary());
     }
 }
