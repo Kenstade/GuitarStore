d4abe4f202fcdb503b73f275bffb11a8702c6f93
kenstade <kenstade@mail.ru>
2025-06-07 22:08:40 +0300

    Remove CustomerNotFoundError.cs

--- src/Modules/Customers/GuitarStore.Modules.Customers/Errors/CustomerNotFoundError.cs
+++
@@ -1,9 +0,0 @@
-﻿using BuildingBlocks.Web.Errors;
-
-namespace GuitarStore.Modules.Customers.Errors;
-
-internal sealed class CustomerNotFoundError : NotFoundError
-{
-    public CustomerNotFoundError(Guid id) : base("Not Found", $"Customer with id '{id}' not found.")
-    { }
-}
