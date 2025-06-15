c4eb910c2d6c99002b4fa61cea356731269a1d12
kenstade <kenstade@mail.ru>
2025-06-15 21:48:20 +0300

    Update CartErrors.cs

--- src/Modules/ShoppingCart/GuitarStore.Modules.ShoppingCart/Errors/CartErrors.cs
+++ src/Modules/ShoppingCart/GuitarStore.Modules.ShoppingCart/Errors/CartErrors.cs
@@ -4,5 +4,5 @@ namespace GuitarStore.Modules.ShoppingCart.Errors;
 
 public static class CartErrors
 {
-    public static Error NotFound(Guid id) => Error.NotFound($"Shopping cart not found for current user {id}");
+    public static Error NotFound(Guid id) => Error.NotFound($"Shopping cart not found for user '{id}'.");
 }
