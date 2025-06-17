2dcecbecde9c6003d1485dd146b98bfe217036c5
kenstade <kenstade@mail.ru>
2025-06-17 21:36:37 +0300

    Add Register.cs

---
+++ src/Modules/Identity/GuitarStore.Modules.Identity/Features/Register.cs
@@ -0,0 +1,56 @@
+using BuildingBlocks.Core.Logging;
+using BuildingBlocks.Core.Validation;
+using BuildingBlocks.Web.MinimalApi;
+using FluentValidation;
+using GuitarStore.Modules.Identity.Data;
+using GuitarStore.Modules.Identity.Models;
+using GuitarStore.Modules.Identity.KeyCloak;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace GuitarStore.Modules.Identity.Features;
+
+public sealed record RegisterUserRequest(string Email, string Password);
+internal sealed class Register : IEndpoint
+{
+    public IEndpointRouteBuilder MapEndpoint(IEndpointRouteBuilder builder)
+    {
+        builder.MapPost("users/signup", async (RegisterUserRequest userRequest, IIdentityProvider identityProvider, 
+                IdentityContext dbContext, CancellationToken ct) =>
+        {
+            var result = await identityProvider.RegisterUserAsync(userRequest.Email,"First", "Last", userRequest.Password, ct);
+
+            if (result.IsFailure)
+                return Results.Problem(result.Error);
+           
+            var user = User.Create(userRequest.Email, userRequest.Password, result.Value);
+
+            foreach (var role in user.Roles)
+            {
+                dbContext.Attach(role);
+            }
+            
+            dbContext.Users.Add(user);
+            await dbContext.SaveChangesAsync(ct);
+        
+            return Results.Ok(user.IdentityId);
+        })
+        .AddEndpointFilter<LoggingEndpointFilter<Register>>()
+        .AddEndpointFilter<ValidationEndpointFilter<RegisterUserRequest>>()
+        .WithName("Register")
+        .WithTags("Identity")
+        .AllowAnonymous();
+        
+        return builder;
+    }
+}
+
+public sealed class RegisterUserRequestValidator : AbstractValidator<RegisterUserRequest>
+{
+    public RegisterUserRequestValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Password).NotEmpty();
+    }
+}
