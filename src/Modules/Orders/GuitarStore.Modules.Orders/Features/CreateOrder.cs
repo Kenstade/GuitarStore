5068dc023650705ed1c0bae5601a8b485bfd3595
kenstade <kenstade@mail.ru>
2025-06-19 23:33:39 +0300

    Add CreateOrder.cs

---
+++ src/Modules/Orders/GuitarStore.Modules.Orders/Features/CreateOrder.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Claims;
+using BuildingBlocks.Core.Logging;
+using BuildingBlocks.Core.Security.Authentication;
+using BuildingBlocks.Core.Validation;
+using BuildingBlocks.Web.MinimalApi;
+using FluentValidation;
+using GuitarStore.Modules.Orders.Data;
+using GuitarStore.Modules.Orders.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace GuitarStore.Modules.Orders.Features;
+
+internal sealed record CreateOrderRequest(string AddressId);
+internal sealed class CreateOrder : IEndpoint
+{
+    public IEndpointRouteBuilder MapEndpoint(IEndpointRouteBuilder builder)
+    {
+        builder.MapPost("/orders", async (CreateOrderRequest request, OrdersDbContext dbContext, ClaimsPrincipal claims, 
+            CancellationToken ct) =>
+        {
+            var order = Order.Create(claims.GetUserId(), Guid.Parse(request.AddressId));
+            
+            await dbContext.AddAsync(order, ct);
+            await dbContext.SaveChangesAsync(ct);
+            
+            return Results.Ok($"Order '{order.Id}' placed.");
+        })
+        .WithName("CreateOrder")
+        .WithTags("Orders")
+        .AddEndpointFilter<LoggingEndpointFilter<CreateOrder>>()
+        .AddEndpointFilter<ValidationEndpointFilter<CreateOrderRequest>>()
+        .RequireAuthorization(Constants.Permissions.CreateOrder);    
+        
+        return builder;
+    }
+}
+
+internal sealed class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequest>
+{
+    public CreateOrderRequestValidator()
+    {
+        RuleFor(p => p.AddressId).NotEmpty().Must(id => Guid.TryParse(id, out _)).WithMessage("Invalid Id");
+    }
+}
