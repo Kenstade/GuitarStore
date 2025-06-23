935455ca999273f5ad02f58b8dcd517a5e985e9d
kenstade <kenstade@mail.ru>
2025-06-23 16:47:38 +0300

    Update LoggingEndpointFilter.cs

--- src/BuildingBlocks/BuildingBlocks.Core/Logging/LoggingEndpointFilter.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Logging/LoggingEndpointFilter.cs
@@ -1,31 +1,24 @@
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.Extensions.Logging;
 
 namespace BuildingBlocks.Core.Logging;
 
-public class LoggingEndpointFilter<TRequest> : IEndpointFilter
+public sealed class LoggingEndpointFilter<TRequest>(ILogger<LoggingEndpointFilter<TRequest>> logger) 
+    : IEndpointFilter
 {
-    private readonly ILogger<LoggingEndpointFilter<TRequest>> _logger;
-
-    public LoggingEndpointFilter(ILogger<LoggingEndpointFilter<TRequest>> logger)
-    {
-        _logger = logger;
-    }
-    
     public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
         const string prefix = nameof(LoggingEndpointFilter<TRequest>);
-        _logger.LogInformation("[{Prefix}] Processing {Request}", prefix ,typeof(TRequest).Name);
+        logger.LogInformation("[{Prefix}] Processing {Request} request.", prefix ,typeof(TRequest).Name);
         
         var result = await next(context);
         if (context.HttpContext.Response.StatusCode == 200)
         {
-            _logger.LogInformation("[{Prefix}] Completed {Request}", prefix, typeof(TRequest).Name);
+            logger.LogInformation("[{Prefix}] Completed {Request}", prefix, typeof(TRequest).Name);
         }
         else
         {
-            _logger.LogError("[{Prefix}] Completed {Request} with {Error}", 
+            logger.LogError("[{Prefix}] Completed {Request} with {Error}", 
                 prefix, typeof(TRequest).Name, context.HttpContext.Response.StatusCode);
         }
         
