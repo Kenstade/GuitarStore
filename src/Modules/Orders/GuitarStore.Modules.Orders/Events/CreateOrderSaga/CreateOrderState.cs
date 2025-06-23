483d7c577b0896b4ea7f29a0da1dc2f1c4169fa5
kenstade <kenstade@mail.ru>
2025-06-23 16:56:25 +0300

    Add CreateOrder saga

---
+++ src/Modules/Orders/GuitarStore.Modules.Orders/Events/CreateOrderSaga/CreateOrderState.cs
@@ -0,0 +1,13 @@
+using MassTransit;
+
+namespace GuitarStore.Modules.Orders.Events.CreateOrderSaga;
+
+public sealed class CreateOrderState : SagaStateMachineInstance, ISagaVersion
+{
+    public Guid CorrelationId { get; set; }
+    public int Version { get; set; }
+    public string CurrentState { get; set; } = null!;
+    public int Validated { get; set; }
+    
+    public Guid OrderId { get; set; }
+}
