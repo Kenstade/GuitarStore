bfa74490fee014c443e04049dee659f0f3e9d479
kenstade <kenstade@mail.ru>
2025-06-23 17:38:00 +0300

    Fix swagger 404 error

--- src/Api/GuitarStore.Api/Program.cs
+++ src/Api/GuitarStore.Api/Program.cs
@@ -74,6 +74,7 @@ app.MapOpenApi();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("openapi/v1.json", "v1");
+    options.RoutePrefix = string.Empty;
 });
 
 app.UseHangfireDashboard();
