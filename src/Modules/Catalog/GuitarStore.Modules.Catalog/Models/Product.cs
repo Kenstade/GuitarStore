bbaf55cb08aba9230b69f5b658edb878259ecfbd
kenstade <kenstade@mail.ru>
2025-06-23 16:22:39 +0300

    Update Product.cs

--- src/Modules/Catalog/GuitarStore.Modules.Catalog/Models/Product.cs
+++ src/Modules/Catalog/GuitarStore.Modules.Catalog/Models/Product.cs
@@ -15,9 +15,9 @@ internal sealed class Product : Aggregate<Guid>
     public string? Description { get; private set; }
     public decimal Price { get; private set; }
     public ProductColor Color { get; private set; }
-    public int Stock { get; private set; }
-    public bool IsAvailable { get; private set; }
     public int AvailableStock { get; private set; }
+    public int? ReservedStock { get; private set; }
+    public bool IsAvailable { get; private set; }
     public int CategoryId { get; private set; }
     public Category Category { get; private set; } = null!;
     public int BrandId { get; private set; } 
@@ -62,44 +62,50 @@ internal sealed class Product : Aggregate<Guid>
         _images.AddRange(images);
     }
 
-    public string? GetMainImage()
-    {
-        var mainImage = _images.FirstOrDefault(p => p.IsMain);
-        
-        return mainImage?.ImageUrl;
-    }
-    public int RemoveStock(int quantity)
-    {
-        if (Stock < quantity)
-        {
-            throw new InsufficientStockException(
-                $"Empty stock, product item '{Name}' with quantity {quantity} is not available.");
-        }
-        int removed = Math.Min(quantity, Stock);
+    public string? GetMainImage() => _images.FirstOrDefault(p => p.IsMain)?.ImageUrl;
 
-        Stock -= removed;
-        if(Stock == 0) IsAvailable = false;
+    public bool HasAvailableStock(int quantity)
+    {
+        if (quantity < 0) quantity *= -1;
         
-        return removed;
+        return AvailableStock >= quantity;
+    }
+
+    public void ReserveStock(int quantity)
+    {
+        if (quantity < 0) quantity *= -1;
+        
+        RemoveStock(quantity);
+        
+        ReservedStock += quantity;
+    }
+    
     public void RemoveStock(int quantity)
     {
-        if (AvailableStock < quantity)
-        {
-            throw new ProblemDetailsException(ProductErrors.InsufficientStock(Id, quantity));
-        }
-
         AvailableStock -= quantity;
-        if(AvailableStock == 0) IsAvailable = false;
+        
+        if(AvailableStock == 0)
+            Deactivate();
     }
+
+    public void RestoreStock(int quantity)
+    {
+        if (quantity < 0) quantity *= -1;
+        
+        AvailableStock += quantity;
+    }
+    
+    public void Activate() => IsAvailable = true;
+    public void Deactivate() => IsAvailable = false;
 }
 
 internal sealed class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
     public void Configure(EntityTypeBuilder<Product> builder)
     {
-        builder.ToTable("products");
-
-        builder.HasKey(p => p.Id);
+        builder.ToTable("products")
+            .HasKey(p => p.Id);
+        
         builder.Property(p => p.Id)
             .ValueGeneratedNever();
 
