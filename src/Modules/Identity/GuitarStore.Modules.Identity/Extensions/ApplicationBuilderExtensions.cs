ee41739e4dddc584ecb55df3809d1020d8bd5fb4
kenstade <kenstade@mail.ru>
2025-06-23 16:24:47 +0300

    Update hangfire jobs registration

--- src/Modules/Identity/GuitarStore.Modules.Identity/Extensions/ApplicationBuilderExtensions.cs
+++ src/Modules/Identity/GuitarStore.Modules.Identity/Extensions/ApplicationBuilderExtensions.cs
@@ -12,11 +12,11 @@ internal static class ApplicationBuilderExtensions
 {
     public static IApplicationBuilder UseBackgroundJobs(this IApplicationBuilder app, IConfiguration configuration)
     {
-        var hangfireOptions = configuration.GetOptions<HangfireOptions>($"{IdentityModule.ModuleName}:{nameof(HangfireOptions)}");
+        var hangfireOptions = configuration.GetOptions<HangfireOptions>($"{Constants.ModuleName}:{nameof(HangfireOptions)}");
         
         app.ApplicationServices
             .GetRequiredService<IRecurringJobManager>()
-            .AddOrUpdate<ProcessOutboxMessageJob>("identity-outbox-processor", job => 
+            .AddOrUpdate<ProcessOutboxMessagesJob>($"{Constants.ModuleName}-outbox-processor", job => 
                 job.ProcessAsync(), hangfireOptions.OutboxSchedule);
         
         return app;
--- src/Modules/Orders/GuitarStore.Modules.Orders/Extensions/ApplicationBuilderExtensions.cs
+++ src/Modules/Orders/GuitarStore.Modules.Orders/Extensions/ApplicationBuilderExtensions.cs
@@ -12,11 +12,11 @@ internal static class ApplicationBuilderExtensions
 {
     public static IApplicationBuilder UseBackgroundJobs(this IApplicationBuilder app, IConfiguration configuration)
     {
-        var hangfireOptions = configuration.GetOptions<HangfireOptions>($"{OrdersModule.ModuleName}:{nameof(HangfireOptions)}");
+        var hangfireOptions = configuration.GetOptions<HangfireOptions>($"{Constants.ModuleName}:{nameof(HangfireOptions)}");
         
         app.ApplicationServices
             .GetRequiredService<IRecurringJobManager>()
-            .AddOrUpdate<ProcessOutboxMessageJob>("orders-outbox-processor", job => 
+            .AddOrUpdate<ProcessOutboxMessagesJob>($"{Constants.ModuleName}-outbox-processor", job => 
                 job.ProcessAsync(), hangfireOptions.OutboxSchedule);
         
         return app;
