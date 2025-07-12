218956b2ef80f22f8fe5c85c3e2979bc8e6d3f03
kenstade <kenstade@mail.ru>
2025-07-12 15:45:12 +0300

    Update GetProductDetails.cs

--- src/Modules/Catalog/MusicStore.Modules.Catalog/Features/GetProductDetails.cs
+++ src/Modules/Catalog/MusicStore.Modules.Catalog/Features/GetProductDetails.cs
@@ -1,12 +1,12 @@
+using BuildingBlocks.Core.Dapper;
 using BuildingBlocks.Core.Logging;
 using BuildingBlocks.Core.Validation;
 using BuildingBlocks.Web.MinimalApi;
+using Dapper;
 using FluentValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.EntityFrameworkCore;
-using MusicStore.Modules.Catalog.Data;
 using MusicStore.Modules.Catalog.Errors;
 
 namespace MusicStore.Modules.Catalog.Features;
@@ -18,25 +18,35 @@ internal sealed class GetProductDetails : IEndpoint
     public IEndpointRouteBuilder MapEndpoint(IEndpointRouteBuilder builder)
     {
         builder.MapGet("catalog/{id}", async ([AsParameters]GetProductDetailsRequest request, 
-            CatalogDbContext dbContext, CancellationToken ct) =>  
+                IDbConnectionFactory dbConnectionFactory,CancellationToken ct) =>  
         {
-            var product = await dbContext.Products
-                .AsNoTracking()
-                .Where(p => p.Id == Guid.Parse(request.Id))
-                .Select(p => new GetProductDetailsResponse
-                (
-                    p.Name,
-                    p.Description,
-                    p.Price,
-                    p.Category.Name,
-                    p.Brand.Name,
-                    p.Specifications
-                    .Select(ps => new ProductSpecSummary
-                    (
-                        ps.SpecificationType.Name, 
-                        ps.Value
-                    )).ToList()
-                )).FirstOrDefaultAsync(ct);
+            await using var connection = await dbConnectionFactory.OpenConnectionAsync();
+
+            const string sql =
+                $"""
+                 SELECT 
+                 p.name as {nameof(GetProductDetailsResponse.Name)},
+                 p.description as {nameof(GetProductDetailsResponse.Description)},
+                 p.price as {nameof(GetProductDetailsResponse.Price)},
+                 c.name as {nameof(GetProductDetailsResponse.Category)},
+                 b.name as {nameof(GetProductDetailsResponse.Brand)},
+                 (
+                    SELECT jsonb_agg(jsonb_build_object(
+                    'Type', st.name,
+                    'Value', ps.value
+                    ))
+                    FROM catalog.product_specification ps
+                    JOIN catalog.specification_type st ON ps.specification_type_id = st.id
+                    WHERE ps.product_id = p.id
+                 ) as Specs
+                 FROM catalog.products p
+                 JOIN catalog.categories c ON p.category_id = c.id
+                 JOIN catalog.brands b ON p.brand_id = b.id
+                 WHERE p.id = @requestId::uuid
+                 """;
+            
+            var requestId = Guid.Parse(request.Id);
+            var product = await connection.QueryFirstOrDefaultAsync(sql, new { requestId });
 
             return product is not null 
                 ? Results.Ok(product)
